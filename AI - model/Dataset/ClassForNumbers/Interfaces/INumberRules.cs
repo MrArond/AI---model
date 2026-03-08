b1d84ee3cba33b139e594c86d1c98be422024398
Jakub <Jakubr14@gmail.com>
2026-03-08 14:20:14 +0100

    test

---
+++ AI - model/Dataset/ClassForNumbers/Interfaces/INumberRules.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AI___model.Dataset.ClassForNumbers.Interfaces
+{
+    internal interface INumberRules
+    {
+    }
+}
