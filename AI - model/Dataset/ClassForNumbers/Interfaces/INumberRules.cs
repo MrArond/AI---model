668f3373c6a52076d9964bce37344bf947bff8d4
MrArond <Jakubr14@gmail.com>
2026-03-08 19:57:29 +0100

    Merge

---
+++ AI - model/Dataset/ClassForNumbers/Interfaces/INumberRules.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AI___model.Dataset.ClassForNumbers.Interfaces
+{
+    internal interface INumberRules
+    {
+        public Task<byte> NumberRules(bool[,]Table); 
+    }
+}
