b529c285176d395eb2753ab3e4324c184c5c083b
MrArond <Jakubr14@gmail.com>
2026-03-17 22:48:42 +0100

    changes

---
+++ AI - model/Dataset/ClassForNumbers/Implementation/Rules.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI___model.Dataset.ClassForNumbers.Implementation
+{
+    internal class Rules : Interfaces.IRules
+    {
+        async Task<int> Interfaces.IRules.Field(List<bool[,]> images)
+        {
+            int area = 0;
+
+            foreach (var image in images)
+            {
+                int rows = image.GetLength(0);
+                int cols = image.GetLength(1);
+
+                for (int y = 0; y < rows; y++)
+                {
+                    for (int x = 0; x < cols; x++)
+                    {
+                        if (image[y, x])
+                        {
+                            area++;
+                        }
+                    }
+                }
+            }
+
+            if (area == 0)
+                return 0;
+
+            return area;
+        }
+        async Task<float> Interfaces.IRules.Circuit(List<bool[,]> images)
+        {
+            float perimeter = 0;
+
+            foreach (var image in images)
+            {
+                int rows = image.GetLength(0);
+                int cols = image.GetLength(1);
+
+                for (int y = 0; y < rows; y++)
+                {
+                    for (int x = 0; x < cols; x++)
+                    {
+                        if (image[y, x])
+                        {
+                            if (y == 0 || !image[y - 1, x]) perimeter++;
+                            if (y == rows - 1 || !image[y + 1, x]) perimeter++;
+                            if (x == 0 || !image[y, x - 1]) perimeter++;
+                            if (x == cols - 1 || !image[y, x + 1]) perimeter++;
+                        }
+                    }
+                }
+            }
+
+            return perimeter;
+        }
+        Task<float> Interfaces.IRules.Width(List<bool[,]> images)
+        {
+            throw new NotImplementedException();
+        }
+        Task<float> Interfaces.IRules.Height(List<bool[,]> images)
+        {
+            throw new NotImplementedException();
+        }
+        Task<float> Interfaces.IRules.Symetry(List<bool[,]> images)
+        {
+            throw new NotImplementedException();
+        }
+        Task<float> Interfaces.IRules.Proportion(List<bool[,]> images)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AI - model/Dataset/ClassForNumbers/Interfaces/IRules.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI___model.Dataset.ClassForNumbers.Interfaces
+{
+    internal interface IRules
+    {
+        Task<int> Field(List<bool[,]> images);
+        Task<float> Circuit(List<bool[,]> images);
+        Task<float> Width(List<bool[,]> images);
+        Task<float> Height(List<bool[,]> images);
+        Task<float> Symetry(List<bool[,]> images);
+        Task<float> Proportion(List<bool[,]> images);
+
+    }
+}
